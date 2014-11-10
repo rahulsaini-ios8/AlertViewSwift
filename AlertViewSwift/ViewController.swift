ca37a3500e79cf5e8bd989ddd3d5ee148c8c2560
RahulSaini <engineer.rahulsaini@gmail.com>
2014-11-10 12:54:38 +0530

    AlertView iOS 8
    AlertView Example iOS 8 - Swift

--- AlertViewSwift/ViewController.swift
+++ AlertViewSwift/ViewController.swift
@@ -8,7 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIAlertViewDelegate
+{
+    
+    
+    @IBOutlet var rateLabel: UILabel!
+    
+    
+    @IBAction func displayActionSheet(sender: UIButton)
+    {
+        
+        let alertView = UIAlertView(title: "Rate",
+            message: "", delegate:self, cancelButtonTitle: "Cancel", otherButtonTitles: "Rate 1 Star", "Rate 2 Stars",
+            "Rate 3 Starts", "Rate 4 Stars", "Rate 5 Stars");
+        alertView.alertViewStyle = .Default
+        alertView.show()
+    }
+    
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int)
+    {
+        switch buttonIndex
+        {
+        case 1:
+            rateLabel.text = "You've rated 1 star.";
+            break;
+        case 2:
+            rateLabel.text = "You've rated 2 stars.";
+            break;
+        case 3:
+            rateLabel.text = "You've rated 3 stars.";
+            break;
+        case 4:
+            rateLabel.text = "You've rated 4 stars.";
+            break;
+        case 5:
+            rateLabel.text = "You've rated 5 stars.";
+            break;
+        default:
+            rateLabel.text = "You haven't rate this app yet.";
+            break;
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
